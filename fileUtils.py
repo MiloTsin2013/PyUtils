eb48224ec8d99f1ab8c34139d38a609d54d73838
endRuz <endruz@foxmail.com>
2018-12-16 21:45:04 +0800

    序列化

--- fileUtils.py
+++ fileUtils.py
@@ -1,6 +1,7 @@
 import codecs
 import chardet
 import os
+import pickle
 
 def getCode1(file):
     '''
@@ -17,7 +18,8 @@ def getCode1(file):
 
 def getCode2(file):
     '''
-    以二进制的形式分行获取文件内容，分析文件编码格式，当达到高准确率时，返回编码格式，适用于大文件。
+    以二进制的形式分行获取文件内容，分析文件编码格式。
+    当达到高准确率时，返回编码格式，适用于大文件。
     '''
     print("Getting code type...")
     detector = chardet.universaldetector.UniversalDetector()
@@ -33,26 +35,21 @@ def getCode2(file):
     return code
 
 def getSize(file):
-    '''
-    获取文件的大小,结果保留两位小数，单位为MB
-    '''
+    '''获取文件的大小,结果保留两位小数，单位为MB'''
     size = os.path.getsize(file)
     size = size/float(1024*1024)
     return round(size, 2) # 四舍五入，保留两位小数点
 
-def convert(file, encoding="UTF-8"):
-    '''
-    将指定文件转换到指定格式，默认的是转到 UTF-8
-    file:    文件路径
-    encoding:  输出文件格式
-    '''
+def convert(file, codeType=None, encoding="UTF-8"):
+    '''将指定文件转换到指定格式，默认的是转到 UTF-8'''
     print("Start")
-    if getSize(file) <= 1:
-        print('Small File...')
-        codeType = getCode1(file)
-    else:
-        print('Big File...')
-        codeType = getCode2(file)
+    if codeType == None:
+        if getSize(file) <= 1:
+            print('Small File...')
+            codeType = getCode1(file)
+        else:
+            print('Big File...')
+            codeType = getCode2(file)
     print("Convert {}".format(file))
     print("From {} to {} ...".format(codeType, encoding))
     with codecs.open(file, 'r', codeType, errors="ignore") as f:
@@ -62,9 +59,7 @@ def convert(file, encoding="UTF-8"):
     print("Over")
 
 def getFileNames(filePath):
-    '''
-    获取文件夹下所有文件的名字
-    '''
+    '''获取文件夹下所有文件的名字'''
     fileNames = []
     files = os.listdir(filePath)	# 得到文件夹下的所有文件名称
     # print(files)
@@ -77,15 +72,24 @@ def getFileNames(filePath):
     return fileNames
 
 def getFolderNames(folderPath):
-    '''
-    获取文件夹下所有文件夹的名字
-    '''
+    '''获取文件夹下所有文件夹的名字'''
     folderNames = []
     files=os.listdir(folderPath)	# 得到文件夹下的所有文件名称
     for file in files:	# 遍历文件夹
         if os.path.isdir("{}//{}".format(folderPath, file)):	# 判断是否是文件夹
             folderNames.append(file)
     return folderNames
+
+def serializate(self,variable,file):
+    '''进行序列化操作'''
+    with open(file,'wb') as f:
+        pickle.dump(variable,f)
+
+def deserializate(self,file):
+    '''进行反序列化操作'''
+    with open(file,'rb') as f:
+        variable = pickle.load(f)
+    return variable
 
 if __name__ == "__main__":
     pass
